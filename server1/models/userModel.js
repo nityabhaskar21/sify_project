4e369865b2c518c0d6d80ed14da526e28c7fcf42
nitya <nityabhaskar21@gmail.com>
2020-12-25 11:50:41 +0530

    added email match validation

--- server1/models/userModel.js
+++ server1/models/userModel.js
@@ -8,7 +8,8 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true, 'Email is required!']
+    required: [true, 'Email is required!'],
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
   fname: {
     type: String,
