40ab88bc55e8f83eff7c44b1836da88d64d566c3
HarikaCherukuvada <harika.cherukuvada@gmail.com>
2020-12-28 17:08:59 +0530

    orders

--- server3/mongodb.js
+++ server3/mongodb.js
@@ -2,7 +2,8 @@ let mongoose = require('mongoose');
 let mongoClient = require('mongodb').MongoClient;
 let Test = require('./models/testModel');
 let User = require('./models/userModel');
-let Order = require('./models/orderModel')
+let Order = require('./models/orderModel');
+let Profile = require('./models/profileModel')
 
 const DB = "mongodb+srv://harika3003:passsify3003@cluster0.tfqsz.mongodb.net/test?retryWrites=true&w=majority"
 const options = {
@@ -12,27 +13,132 @@ const options = {
   useUnifiedTopology: true
 }
 
-//viewall orders from ordermodel
-module.exports.viewAllOrder = (cb) => {
+//view orders by ratings
+module.exports.viewOrderbyrating = (cb) => {
   mongoose.connect(DB, options, (err, client) => {
+    var resjson = {};
     //  console.log('Client', client)
     if (!err) {
       console.log("Success!")
-      Order.find((err, doc) => {
+      Order.find({
+        ratings: ratings
+      }, (err, data) => {
         if (err) {
-          var resjson = {
-            "error": err.errors || 'orders not available'
-          }
+           resjson = {
+           msg: 'orders not available'
+          };
         } else {
-          console.log('doc: ', doc)
+          data.forEach(doc => {
+          console.log('Info of orders', doc);
           resjson = {
-            ...doc
-          }
-        }
+            msg: 'success', doc
+          };
+        })
+      }
         mongoose.connection.close()
         cb(resjson)
       })
 
     } else console.log("ERROR!:", err.message)
   })
-}
+};
+
+//view orders by username
+module.exports.vieworderbyusername = (cb, uname) => {
+  mongoose.connect(DB, options, (err, client) => {
+    var resjson = {};
+    //  console.log('Client', client)
+    if (!err) {
+      console.log('Success!');
+      Order.find({
+          username: uname
+        },
+        (err, data) => {
+          if (err) {
+            resjson = {
+              msg: 'User not available'
+            };
+          } else {
+            data.forEach(doc => {
+              console.log('Info of order', doc);
+              resjson = {
+                msg: 'success', doc
+              };
+              Profile.find({
+                  userid: doc._id
+                },
+                (perr, pdata) => {
+                  if (perr) {
+                    console.log('error occured', perr);
+                    resjson = {
+                      ...resjson,
+                      pdata: 'No user profile available'
+                    };
+                    console.log('new resjson: ', resjson);
+                  } else {
+                    pdata.forEach(d => {
+                      console.log('User Profile: ', d);
+                      resjson.pdata = d;
+                    });
+                  }
+                  cb(resjson);
+                }
+              );
+            });
+          }
+        }
+      );
+    } else console.log('ERROR!:', err.message);
+  });
+};
+
+//add orders for productid
+module.exports.addProductid = (cb, productjson) => {
+  mongoose.connect(DB, options, (err, client) => {
+    var resjson = {}
+    //  console.log('Client', client)
+    if (!err) {
+      console.log("Success!")
+      Order.find({
+        "productid": productjson.productid
+      }, (err, data) => {
+        if (err) {
+          resjson = {
+            "msg": "Order not available"
+          }
+        } else {
+          data.forEach(doc => {
+            console.log('Info of order', doc)
+            resjson = {
+              "msg": "success",
+              doc
+            }
+            console.log(typeof doc._id)
+            console.log(productjson)
+            
+            delete productjson.productid;
+            Product.create({
+              merchaneid: mongoose.Types.ObjectId(doc._id),
+              ...productjson
+            }, (qerr, qdata) => {
+              if (qerr) {
+                console.log('Error in creating product', qerr)
+                //console.log(perr)
+                resjson = {
+                  "msg": "Product failed to create "
+                }
+              } else {
+                resjson = {
+                  "msg": "Product created sucessfully",
+                  qdata
+                }
+              }
+              cb(resjson)
+            })
+          })
+        }
+        //  mongoose.connection.close();
+      })
+    } else console.log("ERROR!:", err.message)
+  })
+};
--- server3/routes/orders.js
+++ server3/routes/orders.js
@@ -8,14 +8,51 @@ var cors = require('cors')
 //});
 
 
-//get orders
-router.get('/viewall', cors({
+//get orders by ratings
+router.get('/viewordersbyrating', cors({
   origin: "http://localhost:4200"
 }), function (req, res, next) {
-
-  mongodao.viewallOrder(function (result) {
+  let ratings = req.params.ratings;
+  mongodao.viewOrderbyrating(function (result) {
     res.send(result)
-  })
+  }, ratings)
+});
+
+//get orders by username
+router.get('/viewordersbyusername', cors({
+  origin: "http://localhost:4200"
+}), function (req, res, next) {
+  let username = req.params.uname;
+  mongodao.vieworderbyusername(function (result) {
+    res.send(result)
+  }, username)
+});
+
+//add orders for productid
+router.options('/addproductid', cors())
+router.post('/addproductid', cors({
+  origin: "http://localhost:4200"
+}), function (req, res, next) {
+  let productid = req.params.productid;
+  let merchantid = req.params.merchantid;
+  let buyerid = req.params.buyerid;
+  let review = req.params.review;
+  let orderstatus = req.params.orderstatus;
+  let iscancelled = req.params.iscancelled;
+  let rating = req.params.rating;
+  
+  let productjson = {
+    productid,
+    merchantid,
+    buyerid,
+    review,
+    orderstatus,
+    iscancelled,
+    rating
+  }
+  mongodao.addProductid (function (result) {
+    res.send(result)
+  }, productjson)
 });
 
 module.exports = router;
